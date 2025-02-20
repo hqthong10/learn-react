98cd388b9c9dab576e9578e430aea1c3ddc02048
Quang Thông <hq.thong10@gmail.com>
2025-02-20 16:50:39 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [list, setList] = useState<any[]>([]);
+  const [words, setWords] = useState<any[]>([]);
+
+  
+
+  useEffect(() => {
+    setList([{ name: 'name-1' }]);
+
+    fetch('http://localhost:3020/w100',{method: 'get'})
+      .then((res) => res.json())
+      .then((data) => setWords(data.data || []));
+  }, [])
+
+  const addItem = () => {
+    // setList((list) => list.push({ name: `name-${list.length + 1}` }));
+    setList([ ...list, { name: `name-${list.length + 1}` }]);
+  }
+
+  const removeItem = (item: any) => {
+    // setList((list) => list.push({ name: `name-${list.length + 1}` }));
+    const _lis = list.filter((obj) => obj.name !== item.name);
+    setList(_lis);
+  }
+
+  const listEle = list.map((item: any) => {
+    return <p>{item.name} <button onClick={() => removeItem(item)}>remove</button></p>
+  })
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
+        <div className='list'>
+          {listEle}
+        </div>
+
+
+        {words.length}
+
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+        <button onClick={addItem}>
+          add
+        </button>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
